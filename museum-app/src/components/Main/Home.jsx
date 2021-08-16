743e40cbb098fca4472b40558d0bc629ca8545e8
secolema <secolema@iu.edu>
2021-08-16 19:27:47 -0400

    api

--- museum-app/src/components/Main/Home.jsx
+++ museum-app/src/components/Main/Home.jsx
@@ -6,10 +6,7 @@ function Home(props) {
     return (
         <div className="home_container" >
             <img id="met_image" src={process.env.PUBLIC_URL+'/Museum_images/'+props.image}/>
-            <img id="met_image" src={process.env.PUBLIC_URL+'/Museum_images/'+props.image}/>
-            <img id="met_image" src={process.env.PUBLIC_URL+'/Museum_images/'+props.image}/>
-            <img id="met_image" src={process.env.PUBLIC_URL+'/Museum_images/'+props.image}/>
-            <img id="met_image" src={process.env.PUBLIC_URL+'/Museum_images/'+props.image}/>
+
             
         </div>
         
--- museum-app/src/components/Museums/Met.jsx
+++ museum-app/src/components/Museums/Met.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './Met.css'
 
 function Met() {
+    const url_array="https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=sunflowers"
+
+
+    const [arrayID,setArrayID]=useState([])
+    const [objectID,setObjectID]=useState([])
+
+    useEffect(()=>{
+        fetch(url_array)
+            .then((res) => res.json())
+            .then(res => {
+                setArrayID(res.objectIDs)
+            })
+            .catch(console.error);
+        }
+    )
+    
+    const url_object=`https://collectionapi.metmuseum.org/public/collection/v1/objects/${arrayID[2]}`
+
+    useEffect(()=>{
+        fetch(url_object)
+            .then((res) => res.json())
+            .then(res => {
+                setObjectID(res.primaryImage)
+            })
+            .catch(console.error);
+        }
+
+    )
+    
+    
+
     return (
         <div>
             <div className="grid_container">
                 <div className="block">
-                    <img id = "block_image" src="https://images.metmuseum.org/CRDImages/ad/original/144703.jpg"/>
+                    <img id = "block_image" src={objectID}/>
                 </div>
 
             </div>
