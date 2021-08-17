be81c4bf3af98e56b638335557a9e283dfc7e475
secolema <secolema@iu.edu>
2021-08-17 13:23:10 -0400

    search function

--- museum-app/src/components/Museums/Met.jsx
+++ museum-app/src/components/Museums/Met.jsx
@@ -1,45 +1,69 @@
 import React,{useState, useEffect} from 'react'
 import './Met.css'
 
+import MetForm from './Met/MetForm'
+import Search from './Met/Search'
+
 function Met() {
-    const url_array="https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=sunflowers"
-
-
+    const initialState=""
+    const [searchQuery,setSearchQuery]=useState("")
     const [arrayID,setArrayID]=useState([])
-    const [objectID,setObjectID]=useState([])
 
-    useEffect(()=>{
-        fetch(url_array)
+
+    function getId(url){
+        
+        fetch(url)
             .then((res) => res.json())
             .then(res => {
-                setArrayID(res.objectIDs)
-            })
-            .catch(console.error);
-        }
-    )
-    
-    const url_object=`https://collectionapi.metmuseum.org/public/collection/v1/objects/${arrayID[2]}`
-
-    useEffect(()=>{
-        fetch(url_object)
-            .then((res) => res.json())
-            .then(res => {
-                setObjectID(res.primaryImage)
+                setArrayID(res.objectIDs.slice(0, 12))
             })
             .catch(console.error);
         }
 
-    )
     
+
+    const handleChange = (event)=>{
+        console.log(searchQuery)
+        setSearchQuery({ ...setSearchQuery, [event.target.id]: event.target.value });
+    }
+
+
     
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        // do something with the data in the component state
+        console.log(searchQuery);
+        // clear the form
+
+        let url_array=`https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=${searchQuery.search}`
+        
+        getId(url_array)
+        setSearchQuery(initialState);
+    }
+
 
     return (
         <div>
-            <div className="grid_container">
-                <div className="block">
-                    <img id = "block_image" src={objectID}/>
-                </div>
+            <div>
+                <form onSubmit={handleSubmit} >
+                    <label htmlFor="search">Search for Art: </label>
 
+                    <div className="search_container">
+                        <input
+                            id="search"
+                            type="text"
+                            onChange={handleChange}
+                            value={searchQuery.search}
+                        />
+
+                        <button type="submit" className="search_button">Search</button>
+
+                    </div>
+                    
+                </form>
+            </div>
+            <div className="grid_container">
+                {arrayID!==null && arrayID.map(element=><Search objectID = {element}/>)}
             </div>
         </div>
     )
---
+++ museum-app/src/components/Museums/Met/Search.jsx
@@ -0,0 +1,35 @@
+import React,{useState, useEffect} from 'react'
+
+function Search(props) {
+    const url_object=`https://collectionapi.metmuseum.org/public/collection/v1/objects/${props.objectID}`
+
+
+
+    const [object,setObject]=useState({})
+    useEffect(()=>{
+        fetch(url_object)
+            .then((res) => res.json())
+            .then(res => {
+                setObject(res)
+            })
+            .catch(console.error);
+        }
+    
+
+    
+
+
+
+    
+    )
+    return (
+
+        <div className="block">
+            <img id = "block_image" src={object.primaryImage}/>
+            {object.title!=='' ? <p style={{fontWeight: "bold"}}>'{object.title}'</p>: <p style={{fontWeight: "bold"}}>'Unknown'</p>}
+            <p >{object.artistAlphaSort}</p>
+        </div>
+    )
+}
+
+export default Search
